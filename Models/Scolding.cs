d607bd72a3d8b3b3e8135325bc82ad779b848bfd
beinhorn1990 <71242936+beinhorn1990@users.noreply.github.com>
2021-07-05 21:50:09 -0400

    Update Scolding.cs
    final changes

--- Models/Scolding.cs
+++ Models/Scolding.cs
@@ -1,11 +1,13 @@
 using System;
+
 namespace TamagotchiAPIJune29.Models
 {
-public class Scolding
-{
-    public int Id { get; set; }
-    public DateTime When { get; set; }
-    public int PetID { get; set; }
-    public Pet Pet { get; set; }
-}
+    public class Scolding
+    {
+        public int Id { get; set; }
+
+        public DateTime When { get; set; }
+
+        public int PetId { get; set; }
+    }
 }
