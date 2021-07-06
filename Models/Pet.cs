6200eb08c2d46f9230c4a274e2c3f1c3d2d0effa
beinhorn1990 <71242936+beinhorn1990@users.noreply.github.com>
2021-07-05 21:47:34 -0400

    Update Pet.cs
    final change

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,12 +1,28 @@
 using System;
+using System.Collections.Generic;
+
 namespace TamagotchiAPIJune29.Models
 {
-public class Pet
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public DateTime Birthday { get; set; }
-    public int HungerLevel { get; set; }
-    public int HappinessLevel { get; set; }
-}
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; } = DateTime.Now;
+
+        public int HungerLevel { get; set; } = 0;
+
+        public int HappinessLevel { get; set; } = 0;
+
+        public DateTime LastInteractedWithDate { get; set; }
+
+        public bool IsDead { get; set; } = false;
+
+        public List<Playtime> Playtimes { get; set; }
+
+        public List<Feeding> Feedings { get; set; }
+
+        public List<Scolding> Scoldings { get; set; }
+
+    }
 }
